ab76c08f22522000cb06bda82e16a04a67162fec
Vijay <vijayanarasimha2001@gmail.com>
2022-04-19 19:51:21 +0530

    deploy

--- studyroom/settings.py
+++ studyroom/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/statics/'
+STATIC_URL = '/statics/admin'
 MEDIA_URL= '/images/'
 #STATICFILES_DIRS=['statics']
 STATIC_ROOT=os.path.join(BASE_DIR,'statics')
