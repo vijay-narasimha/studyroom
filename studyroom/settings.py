2793bf7a1e4c6a80f53c168f65596ae1c6aeab4e
Akash <kollabathulaakash195@gmail.com>
2022-04-19 06:00:41 +0530

    deploy

--- studyroom/settings.py
+++ studyroom/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path,os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,10 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'statics/'
 MEDIA_URL= '/images/'
-STATICFILES_DIRS=[BASE_DIR/'statics']
-
+#STATICFILES_DIRS=['statics']
+STATIC_ROOT=os.path.join(BASE_DIR,'statics')
 
 MEDIA_ROOT=BASE_DIR / 'statics/images'
 # Default primary key field type
